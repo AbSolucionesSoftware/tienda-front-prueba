d2c9c954554d6fb22e7110bef1d1c5e8c2da0b31
Diego <consultoresartificialbrain@gmail.com>
2020-11-24 11:22:54 -0600

    agregando recuperacion de pass

--- src/pages/users/Pedidos/pedidos.js
+++ src/pages/users/Pedidos/pedidos.js
@@ -144,7 +144,6 @@ export default function PedidosUsuario(props) {
 					setEstado(true);
                 })
                 .catch((err) => {
-                    console.log(err.response)
                     notification.error({
                         message: 'Error del servidor',
                         description:
@@ -224,23 +223,15 @@ export default function PedidosUsuario(props) {
 				onCancel={() => {
 					showModal(false);
 				}}
-				footer={[
-					<Button
-						style={{ fontSize: 16 }}
-						type="primary"
-						onClick={() => {
-							showModal(false);
-						}}
-					>
-						Cerrar
-					</Button>
-				]}
+				footer={null}
 			>
+
 				{Elige === true ? (
 					<DetalleApartado detalleApartado={detalleApartado} />
 				) : (
 					<DetallesPedido detallePedido={detallePedido} />
 				)}
+				
 			</Modal>
 		</Spin>
 	);
@@ -448,7 +439,6 @@ function Apartado(props) {
 			<List.Item.Meta
 				avatar={
 					<div className="d-flex justify-content-center align-items-center my-3">
-						<p>Pedido del producto</p>
 						<div className="contenedor-imagen-mostrar-apartado">
 							<img
 								className="imagen-mostrar-apartado"
