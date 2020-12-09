1b54c1ca5e6c356d2198801297f419fafd36f681
Diego <consultoresartificialbrain@gmail.com>
2020-12-09 09:39:16 -0600

    agregando la eliminacion de producto

---
+++ build/precache-manifest.1b56ef2d2001396221983d35f172c1d4.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "566d6fffeb9a1dd606e797cb247e684c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f7250a5d0749e1e04278",
+    "url": "/static/css/2.13cf92e7.chunk.css"
+  },
+  {
+    "revision": "e9d7315a56c14f5f1fc5",
+    "url": "/static/css/main.f2366a52.chunk.css"
+  },
+  {
+    "revision": "f7250a5d0749e1e04278",
+    "url": "/static/js/2.de0c8240.chunk.js"
+  },
+  {
+    "revision": "81f4270d70d06875704b839639c27d8e",
+    "url": "/static/js/2.de0c8240.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9d7315a56c14f5f1fc5",
+    "url": "/static/js/main.3fed9962.chunk.js"
+  },
+  {
+    "revision": "e621ac90fd4b80177548",
+    "url": "/static/js/runtime-main.5aecc905.js"
+  },
+  {
+    "revision": "3c6258e568480fc1e8bd769e42d7579c",
+    "url": "/static/media/bg-tarjeta-01.3c6258e5.jpg"
+  },
+  {
+    "revision": "6611645bd370943ebc7139d4062b9377",
+    "url": "/static/media/chip-tarjeta.6611645b.png"
+  },
+  {
+    "revision": "ae0915fe18dcc1bafdd686a15e448ed6",
+    "url": "/static/media/mastercard.ae0915fe.png"
+  },
+  {
+    "revision": "d2eb00b710fa2ee2c6a4408cb42f6fb4",
+    "url": "/static/media/visa.d2eb00b7.png"
+  }
+]);
